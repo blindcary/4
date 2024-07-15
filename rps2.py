c1bdc293b200918520108c4484f3f4f5465d8ecc
blindcary <akiseera@gmail.com>
2024-07-15 14:28:22 +0300

    Update rps2.py

--- rps2.py
+++ rps2.py
@@ -14,5 +14,34 @@ def player_choice():
             return int(player_choice)
         else:
             print("Invalid input, please enter 1,2 or 3.")
+            
 def computer_choice():
     return random.choice([1,2,3])
+
+def winner(player,computer):
+    if(player==RPS.ROCK.value and computer==RPS.SCISSORS.value) or \
+        (player==RPS.PAPER.value and computer==RPS.ROCK.value) or \
+            (player==RPS.SCISSORS.value and computer==RPS.PAPER.value):
+                return "🎉You win!"
+    elif player == computer:
+        return "😲Tie"
+    else:
+        return "🐍Python wins!"
+
+def main():
+    playagain = True
+    while playagain:
+        player = player_choice()
+        computer = computer_choice()
+        print("\nYou chose " + str(RPS(player)).replace('RPS.', '') + ".")
+        print("\nPython chose " + str(RPS(computer)).replace('RPS.', '')+".\n")
+        result = winner(player,computer)
+        print(result)
+        playagain = input("\nPlay again? \nY for yes or \nQ to quit\n\n").lower()
+        if playagain != "y":
+            playagain = False
+    print("\n🎉🎉🎉")
+    print("Thank you for playing!\n")
+
+if __name__ == "__main__":
+    main()
